00e9ec1f5c549e72673265b440e0d1e0c6a2228f
Joseph Nguyen <110655053+janhqnguyen2020@users.noreply.github.com>
2024-12-30 18:14:34 -0800

    Add files via upload

---
+++ login/LoginMoreFeatures/validation.js
@@ -0,0 +1,85 @@
+const form = document.getElementById('form')
+const firstname_input = document.getElementById('firstname-input')
+const email_input = document.getElementById('email-input')
+const password_input = document.getElementById('password-input')
+const repeat_password_input = document.getElementById('repeat-password-input')
+const error_message = document.getElementById("error-message")
+
+form.addEventListener('submit', (e) => {
+    let errors = []
+
+    if(firstname_input){
+        //if we have first_name input then we are in signup page
+        errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_password_input.value)
+    } else {
+        //no firstname input menas we are in login page
+        errors = getLoginFormErrors(email_input.value, password_input.value)
+    }
+
+    if(errors.length > 0)
+    {
+        //if there are any errors
+        e.preventDefault()
+        error_message.innerHTML = errors.join(", ")
+    }
+})
+
+function getSignupFormErrors(firstname, email, password, repeatPassword) {
+    //empty array
+    let errors = []
+
+    if(firstname === '' || firstname == null) {
+        errors.push('First Name is Required')
+        firstname_input.parentElement.classList.add('incorrect')
+    }
+    if(email === '' || email == null) {
+        errors.push('Email is Required')
+        email_input.parentElement.classList.add('incorrect')
+    }
+    if(password === '' || password == null) {
+        errors.push('Password is Required')
+        password_input.parentElement.classList.add('incorrect')
+    }
+
+    if(password.length < 8) {
+        errors.push('Password must have at least 8 characters')
+        password_input.parentElement.classList.add('incorrect')
+    }
+    if(password != repeatPassword) {
+        errors.push('Password does not match repeated password')
+        password_input.parentElement.classList.add('incorrect')
+        repeat_password_input.parentElement.classList.add('incorrect')
+    }
+
+    return errors;
+}
+
+const allinputs = [firstname_input, email_input, password_input, repeat_password_input].filter(input => input != null)
+
+allinputs.forEach(input => {
+    input.addEventListener('input', () => {
+        if(input.parentElement.classList.contains('incorrect')) {
+            input.parentElement.classList.remove('incorrect')
+            error_message.innerText = ''
+        }
+    })
+})
+
+function getLoginFormErrors(email, password)
+{
+    let errors = []
+
+    if(email === '' || email == null) {
+        errors.push('Email is Required')
+        email_input.parentElement.classList.add('incorrect')
+    }
+    if(password === '' || password == null) {
+        errors.push('Password is Required')
+        password_input.parentElement.classList.add('incorrect')
+    } else if (password.length < 8) {
+        errors.push('Password must have at least 8 characters')
+        password_input.parentElement.classList.add('incorrect')
+    }
+
+    return errors;
+}
